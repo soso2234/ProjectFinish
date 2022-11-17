f6b15b11d6e8696a87f4a1b09af9f9580dbc3ee5
soso2234 <mingi371@naver.com>
2022-11-17 18:00:30 +0900

    dd

---
+++ client/src/layouts/Footer.vue
@@ -0,0 +1,43 @@
+<template>
+    <footer>
+        <!--푸터-->
+        <div class="site-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="widget">
+                            <h3>Contact</h3>
+                            <address>파인홈</address>
+                            <ul class="list-unstyled links">
+                                <li><a href="tel://01044510444">111-0444</a></li>
+                                <li><a href="mailto:info@findHome.co.kr">info@findHome.co.kr</a></li>
+                            </ul>
+                        </div>
+                        <!-- /.widget -->
+                    </div>
+                    <!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <div class="widget">
+                            <h3>Links</h3>
+                            <ul class="list-unstyled links">
+                                <li><a href="#">문의</a></li>
+                                <li><a href="#">상담받기</a></li>
+                            </ul>
+                        </div>
+                        <!-- /.widget -->
+                    </div>
+                    <!-- /.col-lg-4 -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container -->
+        </div>
+        <!-- /.site-footer -->
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'footerMenu'
+}
+</script>
